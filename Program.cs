7f0a6e0684b65f0baf75bd8b498193978e7e4748
Runos <Runos@RUNOSTY>
2024-12-12 23:03:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введення значення x з консолі
+        Console.WriteLine("Введіть значення x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        // Одиничні лямбда-вирази для обчислення функції F(x)
+        Func<double, double> F1 = (double xValue) => xValue > 0 ? (xValue * xValue + 4) : 0;
+        Func<double, double> F2 = (double xValue) => xValue > 0 ? (xValue * xValue + 4) : 0;
+
+        // Виконання обчислення залежно від введеного x
+        double result = x > 0 ? F1(x) : F2(x);
+
+        // Виведення результату на консоль
+        Console.WriteLine($"Результат F({x}) = {result}");
+    }
+}
